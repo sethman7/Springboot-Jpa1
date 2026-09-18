19c76498940dfe53d30e2b9b013f02ca2e2c483f
sethman7 <131145377+sethman7@users.noreply.github.com>
2026-09-18 17:52:16 +0900

    ItemDomain

--- src/main/java/jpabook/jpashop/service/MemberService.java
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -4,6 +4,7 @@ package jpabook.jpashop.service;
 
 import jpabook.jpashop.domain.Member;
 import jpabook.jpashop.repository.MemberRepository;
+import lombok.RequiredArgsConstructor;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -12,10 +13,11 @@ import java.util.List;
 
 @Service
 @Transactional(readOnly=true)
+@RequiredArgsConstructor
 public class MemberService {
 
     @Autowired
-    private MemberRepository memberRepository;
+    private final MemberRepository memberRepository;
 
     /*
      * 회원 가입
