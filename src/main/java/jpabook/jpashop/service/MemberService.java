21b2d44b5859399368b270f83f1a52c2ddf9d6eb
sethman7 <131145377+sethman7@users.noreply.github.com>
2026-09-18 14:00:48 +0900

    MemberServiceTest

---
+++ src/main/java/jpabook/jpashop/service/MemberService.java
@@ -0,0 +1,49 @@
+package jpabook.jpashop.service;
+
+
+
+import jpabook.jpashop.domain.Member;
+import jpabook.jpashop.repository.MemberRepository;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import java.util.List;
+
+@Service
+@Transactional(readOnly=true)
+public class MemberService {
+
+    @Autowired
+    private MemberRepository memberRepository;
+
+    /*
+     * 회원 가입
+     */
+    //회원 가입
+    @Transactional
+    public Long join(Member member) {
+        validateDuplicateMember(member);
+        memberRepository.save(member);
+        return member.getId();
+    }
+
+    //중복 회원 검증
+    private void validateDuplicateMember(Member member) {
+        //exception
+        List<Member> findMembers = memberRepository.findByName(member.getName());
+        if (!findMembers.isEmpty()) {
+            throw new IllegalStateException("Member already exists");
+        }
+    }
+
+    //회원 전체 조회
+    public List<Member> findAll() {
+        return memberRepository.findAll();
+    }
+
+    public Member findOne(Long id) {
+        return memberRepository.findOne(id);
+    }
+
+}
